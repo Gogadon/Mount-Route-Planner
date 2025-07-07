3e4ef32a80681982020ec9467190ac6c6325b74e
Gogadon <58675646+Gogadon@users.noreply.github.com>
2025-07-07 19:00:41 +0200

    Update MRP_DB.lua

--- MRP_DB.lua
+++ MRP_DB.lua
@@ -1,37 +1,10 @@
-MRP_DB = MRP_DB or {
+-- MRP_DB.lua
+MRP_DB = {
     parsedSteps_A = {},
     currentIndex_A = 0,
     parsedSteps_H = {},
     currentIndex_H = 0,
     autoSkip = true,
     autoAdvance = true,
-    useTomTom = false,
+    useTomTom = true,
 }
-
-if not MRP_DB.parsedSteps_A then
-    MRP_DB.parsedSteps_A = {}
-end
-
-if not MRP_DB.currentIndex_A then
-    MRP_DB.currentIndex_A = 0
-end
-
-if not MRP_DB.parsedSteps_H then
-    MRP_DB.parsedSteps_H = {}
-end
-
-if not MRP_DB.currentIndex_H then
-    MRP_DB.currentIndex_H = 0
-end
-
-if not MRP_DB.autoSkip then
-    MRP_DB.autoSkip = true
-end
-
-if not MRP_DB.autoAdvance then
-    MRP_DB.autoAdvance = true
-end
-
-if not MRP_DB.useTomTom then
-    MRP_DB.useTomTom = false
-end
